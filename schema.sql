37dddcf2c9e8486bc52b40e4b3f156be24947598
Feniuk <143640539+Feniuk@users.noreply.github.com>
2026-04-06 19:11:58 +0200

    tables created

--- schema.sql
+++ schema.sql
@@ -1,6 +1,58 @@
 use shoe_house;
 
-create table test_table (
-    id int auto_increment primary_key,
-    name varchar(100)
-    )
+CREATE TABLE customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    phone VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE categories (
+    name VARCHAR(100) NOT NULL,
+    category_id INT AUTO_INCREMENT PRIMARY KEY
+);
+
+CREATE TABLE products (
+    name VARCHAR(150) NOT NULL,
+    brand VARCHAR(100),
+    price DECIMAL(10,2) NOT NULL,
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
+
+CREATE TABLE warehouse (
+    shelf_id INT AUTO_INCREMENT PRIMARY KEY,
+    product_id INT NOT NULL,
+    size INT NOT NULL,
+    stock_quantity INT NOT NULL,
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+CREATE TABLE orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE order_details (
+    order_item_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    order_price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+CREATE TABLE payments (
+    payment_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    amount DECIMAL(10,2) NOT NULL,
+    payment_method VARCHAR(50), 
+    status VARCHAR(50),
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
